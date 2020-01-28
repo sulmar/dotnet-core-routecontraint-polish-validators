bdaecce8c67a94732535d811f95507d753eb31d1
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-01-28 17:12:43 +0100

    Refaktoring

--- unittests/ValidatorsUnitTests.cs
+++ unittests/ValidatorsUnitTests.cs
@@ -4,73 +4,34 @@ using validators;
 
 namespace unittests
 {
-    public class ValidatorsUnitTests
+
+    public class PeselValidatorTests
     {
-        
-        [Theory]
-        [InlineData("3623981230", true)]
-        [InlineData("9531204591", true)]
-        [InlineData("9542223907", true)]
-        [InlineData("9542223901", false)]
-        [InlineData("5252438106", true)]
-        [InlineData("5851404935", true)]
-        public void NIPValidatorTest(string number, bool expected)
+        private readonly IValidator validator;
+
+        public PeselValidatorTests()
         {
-            IValidator validator = new NIPValidator();
-
-            bool result = validator.IsValid(number);
-
-             Assert.Equal(expected, result);
-
+            validator = new PeselValidator();
         }
 
         [Theory]
         [InlineData("49040501580", true)]
         [InlineData("75012403718", true)]
         [InlineData("46040501580", false)]
-        public void PeselValidatorTest(string number, bool expected)
+        public void IsValid_ValidFormat_ReturnsStatus(string number, bool expected)
         {
-            IValidator validator = new PeselValidator();
-
             bool result = validator.IsValid(number);
 
-             Assert.Equal(expected, result);
-
-        }
-
-        [Fact]
-        public void InvalidNipTest()
-        {
-             IValidator validator = new PeselValidator();
-             Assert.Throws<FormatException>(() => validator.IsValid("954222390"));
-             Assert.Throws<FormatException>(() => validator.IsValid("9542223909999"));
-             Assert.Throws<FormatException>(() => validator.IsValid("AAA954222390"));
+            Assert.Equal(expected, result);
         }
 
 
         [Fact]
-        public void InvalidPeselTest()
+        public void IsValid_InvalidFormat_ThrowsFormatException()
         {
-             IValidator validator = new PeselValidator();
              Assert.Throws<FormatException>(() => validator.IsValid("55555012403718"));
              Assert.Throws<FormatException>(() => validator.IsValid("5012403718"));
              Assert.Throws<FormatException>(() => validator.IsValid("AAA12403718"));
         }
-
-
-        //[Theory]
-        //[InlineData("730295378", true)]
-        //[InlineData("121497515", true)]
-        //[InlineData("015724597", true)]
-        
-        //public void RegonValidatorTest(string number, bool expected)
-        //{
-        //    IValidator validator = new RegonValidator();
-
-        //    bool result = validator.IsValid(number);
-
-        //    Assert.Equal(expected, result);
-
-        //}
     }
 }
